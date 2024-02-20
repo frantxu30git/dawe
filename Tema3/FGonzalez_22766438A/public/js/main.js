4dbba403e8f9855933d35be70ebf74ba99378178
Fran <fragonzcuriel@gmail.com>
2024-02-20 21:15:19 +0100

    terminado

--- Tema3/FGonzalez_22766438A/public/js/main.js
+++ Tema3/FGonzalez_22766438A/public/js/main.js
@@ -2,10 +2,26 @@ import { listaProductosPorTipo } from './tienda.js';
 import { listaNombresProductos } from './tienda.js';
 window.onload = () => {
     function imprimirProductoHTML(producto) {
+        let campoEspecifico = '';
+        let valorCampoEspecifico = '';
+    
+        if (producto.constructor.name === "Ropa") {
+            campoEspecifico = 'Talla';
+            valorCampoEspecifico = producto.getTalla();
+        } else if (producto.constructor.name === "Hogar") {
+            campoEspecifico = 'Tipo';
+            valorCampoEspecifico = producto.getTipo();
+        } else if (producto.constructor.name === "Smartphone") {
+            campoEspecifico = 'Marca';
+            valorCampoEspecifico = producto.getMarca();
+        }
+    
         return `
             <div class="producto" style="border: 1px solid black; margin-bottom: 10px;">
-                <div style="font-weight: bold;">${producto.getNombre()}</div>
+                <div style="font-weight: bold; color: crimson;">${producto.getNombre()}</div>
                 <span style="font-weight: bold;">Precio:</span> ${producto.getPrecio()}€
+                <br>
+                <span style="font-weight: bold;">${campoEspecifico}:</span> ${valorCampoEspecifico}
             </div>
         `;
     }
